804460a90a2892cd88725bd922e87370633b7b9f
Rendy <rendy.de.p@gmail.com>
2020-09-16 16:06:34 +0700

    Add basic authorization

--- pages/_app.js
+++ pages/_app.js
@@ -3,20 +3,47 @@ import {useRouter} from 'next/router'
 import {destroyCookie, parseCookies, setCookie} from "nookies";
 import UserContext from '../components/context/user'
 import User from "../src/user";
-import App from "next";
+import {useEffect} from "react";
 
-export default class MyApp extends App {
-    state = {
-        userKey: ''
+export default function MyApp({Component, pageProps}) {
+    const router = useRouter();
+    const saveLocal = (key, value) => {
+        setCookie(null, key, value, {
+            maxAge: 30 * 24 * 60 * 60,
+            path: '/',
+        })
+    }
+    const getLocal = (key) => {
+        let cookies = getLocals()
+        return cookies[key];
+    }
+    const getLocals = () => {
+        return parseCookies();
+    }
+    const removeLocal = (key) => {
+        destroyCookie(null, key);
+    }
+    const saveUserKey = (userKey) => {
+
+        // Save into cookie.
+        saveLocal('uk', userKey)
+    }
+    const signOut = () => {
+
+        // Remove key from cookie.
+        removeLocal('uk')
+
+        // Force redirect.
+        location.href = '/masuk'
     };
 
-    componentDidMount = async () => {
+    // Get key from cookie.
+    const userKey = getLocal('uk');
+
+    useEffect(() => {
 
         // Define freeAccess Page.
         const freePage = ['/', '/masuk', '/daftar', '/vldseml'];
-
-        // Get key from cookie.
-        const userKey = this.getLocal('uk');
 
         // Make sure that user key is exist.
         if (userKey) {
@@ -28,79 +55,33 @@ export default class MyApp extends App {
                     // Validate result.
                     if (result.data.success) {
 
-                        // Save user key.
-                        this.saveUserKey(userKey)
+                        // Save login data.
+                        saveUserKey(userKey)
                     } else {
 
-                        // Force sign out.
-                        this.signOut();
+                        // Force logout.
+                        signOut()
                     }
                 })
                 .catch(err => {
                     console.log(err)
                 })
         } else {
-
             // Detect if current page is not a free access page.
-            if (!freePage.includes(useRouter().pathname)) {
+            if (!freePage.includes(router.pathname)) {
 
                 // Redirect to login page.
-                await useRouter().push('/masuk')
+                router.push('/masuk')
             }
         }
-    };
-
-    saveUserKey = async (userKey) => {
-
-        // Save key into cookie.
-        this.saveLocal('uk', userKey)
-
-        // Update user state.
-        await this.setState({userKey: userKey});
-    };
-
-    signOut = async () => {
-
-        // Remove key from cookie.
-        this.removeLocal('uk')
-
-        // Update user state.
-        this.setState({userKey: ''});
-    };
-
-    render() {
-        const {Component, pageProps} = this.props;
-
-        return (
-            <UserContext.Provider
-                value={{
-                    userKey: this.state.userKey,
-                    saveUserKey: this.saveUserKey,
-                    signOut: this.signOut
-                }}>
-                <Component {...pageProps} />
-            </UserContext.Provider>
-        );
-    }
-
-    saveLocal(key, value) {
-        // localStorage.setItem(this.prefix + key, value);
-        setCookie(null, key, value, {
-            maxAge: 30 * 24 * 60 * 60,
-            path: '/',
-        })
-    }
-
-    getLocal(key) {
-        let cookies = this.getLocals()
-        return cookies[key];
-    }
-
-    getLocals() {
-        return parseCookies();
-    }
-
-    removeLocal(key) {
-        destroyCookie(null, key);
-    }
+    }, [userKey])
+    return (
+        <UserContext.Provider
+            value={{
+                userKey: userKey,
+                saveUserKey: saveUserKey,
+                signOut: signOut
+            }}>
+            <Component {...pageProps} />
+        </UserContext.Provider>)
 }
